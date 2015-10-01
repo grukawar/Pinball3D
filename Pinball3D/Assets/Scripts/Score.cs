9f3d510eb22204d8a126d03c33ce63d9a1be48b8
jimbo333 <19198@ma-web.nl>
2015-10-01 19:50:37 +0200

    Score script added

---
+++ Pinball3D/Assets/Scripts/Score.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public float score = 0;	
+	public Text countText;
+
+	// Update is called once per frame
+	void Update () {
+		score = score + 1;
+		SetCountText ();
+	}
+
+	void SetCountText (){
+		countText.text = "Score: " + score.ToString();
+		if (score < 0) {
+			score = 0;
+		}
+	}
+}
