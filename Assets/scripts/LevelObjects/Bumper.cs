eecfea61157940b3f6d3d0ce5c7426f94d85ab89
yoshalangius <henkkooi11@gmail.com>
2015-10-14 14:53:21 +0200

    trying to fix the project

---
+++ Assets/scripts/LevelObjects/Bumper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bumper : MonoBehaviour
+{
+    public GameObject poof;
+    public Collider force;
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.name == "ball")
+        {
+            Vector3 scale = transform.localScale;
+            scale.y = 250F; // your new value
+            scale.x = 250F; 
+            scale.z = 250F; 
+            transform.localScale = scale;
+
+
+            Instantiate(poof, transform.position, Quaternion.identity);
+            
+        }
+    }
+
+    void OnCollisionExit(Collision Coll)
+    {
+        Vector3 scale = transform.localScale;
+        scale.y = 150F; // your old value
+        scale.x = 150F; 
+        scale.z = 150F; 
+        transform.localScale = scale;
+    }
+
+}
