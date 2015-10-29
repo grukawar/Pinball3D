3e7ae2319afdc719be53021407658c41ef20ec29
yoshalangius <henkkooi11@gmail.com>
2015-10-29 09:32:46 +0100

    update

---
+++ Assets/scripts/LevelObjects/BigHouse.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigHouse : MonoBehaviour
+{
+    public Transform ball;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ball = other.gameObject.transform;
+            Debug.Log(ball);
+        }
+    }
+
+}
